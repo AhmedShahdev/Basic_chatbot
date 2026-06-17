d0e757efa841e565ecee8c91bafdf3aa6796509c
unknown <you@example.com>
2026-06-17 15:23:59 +0500

    Added Databse Logics

--- run.py
+++ run.py
@@ -2,7 +2,35 @@ import os
 import torch
 from flask import Flask, render_template, jsonify, request
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import psycopg2
 
+# ye mene database k lie code dea hai isme sirf databse dala hua hai
+DB_CONFIG = {
+    "dbname": "chatbot-db",
+    "user" : "postgres",
+    "password" : "8288",
+    "host": "localhost"
+}
+try:
+    conn = psycopg2.connect(**DB_CONFIG)
+    cur = conn.cursor()
+    cur.execute("SELECT count(*) FROM chatdata")
+    print("Database connected! Table count:", cur.fetchone()[0])
+    cur.close()
+    conn.close()
+except Exception as e:
+    print("Connection Failed:", e)
+
+def save_db(role, content):
+    conn = None
+    try:
+        conn = psycopg2.connect(**DB_CONFIG)
+        with conn:
+            with conn.cursor() as cur:
+                cur.execute("INSERT INTO chatdata (role, content) VALUES(%s, %s)", (role, content))
+        print("Saved {role} message to DB")
+    except Exception as e:
+        print(f"Error : {e}")
 
 
 app = Flask(__name__)
@@ -32,6 +60,7 @@ def askchatbot():
         return jsonify({"error": "Message not found"}), 404
     
     try:
+        save_db("user", user_message)
         chat_history.append({
             "role": "user",
             "content": user_message
@@ -44,8 +73,9 @@ def askchatbot():
         full_response = tokenizer.decode(outputs[0], skip_special_tokens = True)
 
         bot_reply = full_response.split("<|assistant|>")[-1].strip()
-
-
+ 
+        save_db("assistant", bot_reply)
+        
         chat_history.append({
             "role" : "assistant",
             "content": bot_reply
@@ -58,21 +88,26 @@ def askchatbot():
     
 
 @app.route('/history', methods=['GET'])
+
 def get_history():
-    formatted_history = []
-    for msg in chat_history:
-        formatted_history.append({
-            "role": msg["role"],
-            "text": msg["content"]
-        })
-    return jsonify({"history": formatted_history})
-    
+        try:             
+            conn = psycopg2.connect(**DB_CONFIG)
+            cur = conn.cursor()
+            cur.execute("SELECT role, content FROM chatdata ORDER BY created_at ASC")
+            rows = cur.fetchall()
+            cur.close()
+            conn.close()
+
+            history = [{"role": row[0], "text": row[1]} for row in rows]
+            return jsonify({"history": history})
+        except Exception as e:
+            return jsonify({"history": [], "error": str(e)})
 
 @app.route('/clear', methods=['POST'])
 def clear_history():
     chat_history.clear()
     return jsonify({"message": "History Cleared"})
 
-
+save_db("system", "Test message: Database is working!")
 if __name__ == '__main__':
     app.run(debug= True)
